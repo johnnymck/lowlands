a27a034a3ff8229c8d93a071a842682c3467c52a
Johnny McKenzie <johnny@Johnnys-iMac.local>
2020-06-28 11:32:20 +0100

    fleshed out landtile class

--- models/land.py
+++ models/land.py
@@ -1,3 +1,30 @@
 """Land.py
 Land models and helpers for simulating land 'Physics'
 """
+
+class LandTile(object):
+    """Land is set at a height from -100 to 100, with water level averaging
+    0 but dynamically changing
+    """
+    self._height
+    self._structure
+
+    def __init__(self, height=0, structs=None):
+        self._height = height
+        self._structures = structs
+    
+    def rise(self, factor):
+        self._height += factor
+        if self._height > 100:
+            self.height = 100
+    
+    def fall(self, factor):
+        self._height -= factor
+        if self._height < -100:
+            self.height = -100
+    
+    def set_asset(self, asset):
+        if self._structure = None:
+            self._structure = asset
+        else:
+            raise Exception
